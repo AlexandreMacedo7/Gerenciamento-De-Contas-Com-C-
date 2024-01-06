9188bd932c8417d4887b6422a4dd78f6575179a8
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-06 17:38:10 -0400

    Cria construtor com herança atributo cpf

--- PessoaFisica.cs
+++ PessoaFisica.cs
@@ -7,6 +7,12 @@ namespace GerenciamentoDeContasComC_
 {
     public class PessoaFisica : Conta
     {
-        public String Cpf;
+        public string Cpf{get; private set;}
+
+        public PessoaFisica(int codigo, int numerodaConta, string titular, string cpf, decimal saldo)
+        :base(codigo, numerodaConta, titular, saldo){
+            Cpf = cpf;
+        }
     }
+    
 }
