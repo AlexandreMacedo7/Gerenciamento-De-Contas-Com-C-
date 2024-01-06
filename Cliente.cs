2065e9f7263203215869344f594f12876eb6f8fa
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-06 15:16:56 -0400

    Cria classe cliente

---
+++ Cliente.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciamentoDeContasComC_
+{
+    public class Cliente
+    {
+        public int Codigo{get; set;}
+        public String Nome {get;set;}
+        public String Cpf {get;set;}
+    }
+}
