9f9a90192bae4ff14f880a5c5f05e1628ccbedec
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-06 17:37:26 -0400

    Adiciona atributos numero da conta, titutar e saldo e metodos de extrato, deposito e saque

--- Conta.cs
+++ Conta.cs
@@ -7,7 +7,31 @@ namespace GerenciamentoDeContasComC_
 {
     public class Conta
     {
-        public int Codigo{get; set;}
-        public String Nome {get;set;}
+        public int Codigo{get; private set;}
+        public int NumeroDaConta {get; private set;}
+        public string Titular {get; private set;}
+        public decimal saldo {get; private set;}
+
+        public Conta(){}
+
+        public Conta(int codigo, int numerodaConta, string titular, decimal saldo){
+            Codigo = codigo;
+            NumeroDaConta = numerodaConta;
+            Titular = titular;
+            saldo = saldo;
+        }
+
+        public decimal ConsultarExtrato(){
+            return saldo;
+        }
+        public decimal RealizarDeposito(decimal deposito){
+            return saldo =+ deposito;
+        }
+        public decimal RealizarSaque(decimal saque){
+            if(saque >= saldo){
+                Console.WriteLine($"Saldo insuficiente para saque no valor de: R$ {saque}");
+            }
+            return saldo - saque;
+        }
     }
 }
